1527a744c5c0fde509a29b6dc6b25658b2e100d8
shz2002 <sj1050437739@gmail.com>
2023-05-13 11:14:18 +0800

    02-C++函数

--- 01.HelloWorld/01.HelloWorld/main.cpp
+++ 01.HelloWorld/01.HelloWorld/main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 
+void MultiplyAndLog(int a, int b)
+{
+	std::cout << a * b << std::endl;
+}
+
 int main()
 {
-	int variable = 8;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "--------------------" << std::endl;
 
-	char a = 'A';
-	char b = 65;
+	MultiplyAndLog(2, 8);
+	MultiplyAndLog(4, 9);
+	MultiplyAndLog(52,66);
 
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-
-	// 5.5  double
-	float variable_a = 5.5; // double
-	double variable_b = 5.5; // double
-	float variable_c = 5.5f; // float
-
-	std::cout << sizeof(bool);
-
-	//std::cout << "Hello World!" << std::endl;
-	std::cout << variable << std::endl;
 	std::cin.get();
 }
