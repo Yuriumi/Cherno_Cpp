28c0f6ba99408be18c2defe7aecc90d6df3e28c6
shz2002 <sj1050437739@gmail.com>
2023-05-15 23:28:57 +0800

    32-C++隐式转换与explicit关键字

--- Instance/Instance/src/main.cpp
+++ Instance/Instance/src/main.cpp
@@ -5,38 +5,32 @@ class Entity
 {
 private:
 	std::string m_Name;
+	int m_Age;
+	bool m_Attack;
 
 public:
-	Entity()
-		: m_Name("Unknow")
+	Entity(const std::string name)
+		: m_Name(name), m_Age(-1), m_Attack(1.0f)
 	{}
 
-	Entity(std::string name)
-		: m_Name(name)
+	Entity(int age)
+		: m_Name("Unknow"), m_Age(age), m_Attack(1.0f)
+	{}
+
+	explicit Entity(bool attack)
+		: m_Name("Unknow"), m_Age(-1), m_Attack(0)
 	{}
 };
 
 int main()
 {
-	int* a = new int;		// 分配4字节的空间
-	int* b = new int[50];	// 分配4 * 50字节的内存空间
+	Entity entity_1(22);
+	Entity entity_2 = 22;
 
-	delete a;
-	delete[] b;
+	Entity entity_a("cherno");
+	// Entity entity_b = "cherno";	ERROR : 隐式转换只能进行一次
+	Entity entity_b = std::string("cherno");
 
-	Entity* e1 = new Entity;	// 我们不需要使用括号,因为它有默认的构造函数
-	Entity* e2 = new Entity[50];
-
-	delete e1;
-	delete[] e2;
-
-	Entity* e = new Entity();
-
-	Entity* e = (Entity*)malloc(sizeof(Entity));
-
-	delete e;
-
-	int* b = new int[50];
-
-	Entity* entity = new(b) Entity("cherno");
+	Entity e1(true);
+	// Entity e2 = "114514";
 }
--- README.md
+++ README.md
@@ -1374,3 +1374,38 @@ int* b = new int[50];
 Entity* e = new(b) Entity();
 ```
 
+## 隐式转换与explicit关键字
+
+### 隐式转换
+
+隐式转换只能进行一次.
+
+``` cpp {.line-numbers}
+Entity entity_1(22);
+Entity entity_2 = 22;
+
+Entity entity_a("cherno");
+// Entity entity_b = "cherno";	ERROR : 隐式转换只能进行一次
+Entity entity_b = std::string("cherno");
+```
+
+我们应该避免这样使用隐式转化,它使代码的可读性大幅度降低.
+
+### explicit关键字
+
+- explicit关键字禁止了编译器的隐式转换.
+- 数学运算中会常常使用;防止数字和向量进行比较.
+
+``` cpp {.line-numebrs}
+class Entity
+{
+    explicit Entity(bool attack)
+        : m_Name("Unknow"), m_Age(-1), m_Attack(0) {}
+};
+
+int main()
+{
+    Entity e1(true);
+    Entity e2 = "114514"; // ERROR : explicit关键字禁止了隐式转换
+}
+```
