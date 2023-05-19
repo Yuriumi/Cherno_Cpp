364e404168ac29d08b1c1a995ab3a30408830676
shz2002 <sj1050437739@gmail.com>
2023-05-19 11:49:32 +0800

    52-C++多维数组

--- 06.HelloWorld/06.HelloWorld/src/main.cpp
+++ 06.HelloWorld/06.HelloWorld/src/main.cpp
@@ -4,6 +4,9 @@
 
 #define LOG(x) std::cout<< x <<std::endl
 
+#define WIDTH 1920
+#define HEIGHT 1080
+
 struct Timer
 {
 	std::chrono::time_point<std::chrono::steady_clock> start, end;
@@ -22,30 +25,87 @@ struct Timer
 	}
 };
 
-void Function()
+struct RGB
+{
+	unsigned int R;
+	unsigned int G;
+	unsigned int B;
+
+	RGB() {}
+
+	RGB(unsigned int r, unsigned int g, unsigned int b) : R(r), G(g), B(b) {}
+};
+
+void Draw_1d()
 {
 	Timer timer;
 
-	for (unsigned int i = 0; i < 100; i++)
+	RGB* rgb = new RGB[WIDTH * HEIGHT];
+
+	for (unsigned int i = 0; i < WIDTH; i++)
 	{
-		std::cout << i << '\n';	// \n的性能要比std::endl高很多
+		for (unsigned int j = 0; j < HEIGHT; j++)
+		{
+			rgb[i * HEIGHT + j] = { 1,1,1 };
+		}
 	}
+
+	delete[] rgb;
+}
+
+void Draw_2d()
+{
+	Timer timer;
+
+	RGB** rgb = new RGB * [WIDTH];
+
+	for (unsigned int x = 0; x < WIDTH; x++)
+	{
+		rgb[x] = new RGB[HEIGHT];
+		for (unsigned int y = 0; y < HEIGHT; y++)
+			rgb[x][y] = { 1,1,1 };
+	}
+
+	for (unsigned int x = 0; x < WIDTH; x++)
+		delete[] rgb[x];
+
+	delete[] rgb;
 }
 
 int main()
 {
-	using namespace std::literals::chrono_literals;	// 使用1s中的's'
+	{
+		Timer timer;
 
-	// 获取当前时间(高精度)
-	std::chrono::time_point<std::chrono::steady_clock> start_a = std::chrono::high_resolution_clock::now();
-	// 类型很长的时候我们可以使用auto关键字
-	auto start = std::chrono::high_resolution_clock::now();
-	std::this_thread::sleep_for(1s);	// 计时时间会大于1s,因为计时本身会有性能开销
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<float> duration = end - start;
-	LOG(duration.count() << 's');
+		int* a1d = new int[5];	// 一维数组,连续的内存块最好的存储方式是存储内存块开头的内存地址.
 
-	Function();
+		int** a2d = new int* [5];
+		for (unsigned int i = 0; i < 5; i++)
+			a2d[i] = new int[5];
 
-	std::cin.get();
+		for (unsigned int x = 0; x < 5; x++)
+		{
+			for (unsigned int y = 0; y < 5; y++)
+				a2d[x][y] = 2;
+		}
+
+		for (unsigned int i = 0; i < 5; i++)
+			delete[] a2d[i];	// 释放真正的数组
+
+		delete[] a2d;	// 释放数组指针,如果直接释放,真正存储数据的数组我们就无法访问到了,会造成内存的泄露.
+
+		// 优化为一维数组
+		int* a2d_1 = new int[4 * 5];
+
+		for (unsigned int x = 0; x < 4; x++)
+		{
+			for (unsigned int y = 0; y < 5; y++)
+			{
+				a2d_1[x * 5 + y] = 2;
+			}
+		}
+	}
+
+	Draw_1d();
+	Draw_2d();
 }
--- README.md
+++ README.md
@@ -2605,3 +2605,107 @@ void Function()
     }
 }
 ```
+
+## 多维数组
+
+数组优化的方法之一是把**多维数组转化为一维数组来进行存储**
+
+``` cpp {.line-numbers}
+int main()
+{
+    int* a1d = new int[5]; // 一维数组,连续的内存块最好的存储方式是存储内存块开头的内存地址.
+
+    int** a2d = new int* [5];
+    for (unsigned int i = 0; i < 5; i++)
+        a2d[i] = new int[5];
+
+    for (unsigned int x = 0; x < 5; x++)
+    {
+        for (unsigned int y = 0; y < 5; y++)
+            a2d[x][y] = 2;
+    }
+
+    for (unsigned int i = 0; i < 5; i++)
+    delete[] a2d[i]; // 释放真正的数组
+
+    delete[] a2d; // 释放数组指针,如果直接释放,真正存储数据的数组我们就无法访问到了,会造成内存的泄露.
+}
+```
+
+可以将二维数组优化为一位数组.
+
+``` cpp {.line-numbers}
+// 优化为一维数组
+int* a2d_1 = new int[4 * 5];
+
+for (unsigned int x = 0; x < 4 ; x++)
+{
+    for (unsigned int y = 0; y < 5; y++)
+    {
+        a2d_1[x * 5 + y] = 2;
+    }
+}
+```
+
+``` cpp {.line-numbers}
+#define WIDTH 1920
+#define HEIGHT 1080
+
+struct RGB
+{
+    unsigned int R;
+    unsigned int G;
+    unsigned int B;
+
+    RGB() {}
+
+    RGB(unsigned int r, unsigned int g, unsigned int b) : R(r), G(g), B(b) {}
+};
+
+void Draw_1d()
+{
+    Timer timer;
+
+    RGB* rgb = new RGB[WIDTH * HEIGHT];
+
+    for (unsigned int i = 0; i < WIDTH; i++)
+    {
+        for (unsigned int j = 0; j < HEIGHT; j++)
+        {
+        rgb[i * HEIGHT + j] = { 1,1,1 };
+        }
+    }
+
+    delete[] rgb;
+}
+
+void Draw_2d()
+{
+    Timer timer;
+
+    RGB** rgb = new RGB * [WIDTH];
+
+    for (unsigned int x = 0; x < WIDTH; x++)
+    {
+        rgb[x] = new RGB[HEIGHT];
+        for (unsigned int y = 0; y < HEIGHT; y++)
+            rgb[x][y] = { 1,1,1 };
+    }
+
+    for (unsigned int x = 0; x < WIDTH; x++)
+        delete[] rgb[x];
+
+    delete[] rgb;
+}
+
+int main()
+{
+    Draw_1d();
+    Draw_2d()
+}
+```
+
+Release模式下
+
+![array](img/array.jpg)
+
